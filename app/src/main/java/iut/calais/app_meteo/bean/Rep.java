285283ed381bbb42be3350a26ee47382fb10162f
vanackere.jeremy <jeremy.vanackere@outlook.fr>
2019-03-12 15:38:05 +0100

    affichage json

---
+++ app/src/main/java/iut/calais/app_meteo/bean/Rep.java
@@ -0,0 +1,13 @@
+package iut.calais.app_meteo.bean;
+
+public class Rep {
+    Coordonnee coord;
+
+    public Coordonnee getCoord() {
+        return coord;
+    }
+
+    public void setCoord(Coordonnee coord) {
+        this.coord = coord;
+    }
+}
--- app/src/main/java/iut/calais/app_meteo/meteo.java
+++ app/src/main/java/iut/calais/app_meteo/meteo.java
@@ -4,7 +4,29 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.google.gson.Gson;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import iut.calais.app_meteo.bean.Coordonnee;
+import iut.calais.app_meteo.bean.Rep;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 public class meteo extends AppCompatActivity {
+
+    public static TextView textLat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,8 +36,35 @@ public class meteo extends AppCompatActivity {
         double latitude = this.getIntent().getDoubleExtra("latitude",0);
         double longitude = this.getIntent().getDoubleExtra("longitude",0);
 
-        TextView textLat = findViewById(R.id.textView);
+        textLat = findViewById(R.id.textView);
 
         textLat.setText("latitude : "+latitude+" / longitude :  "+longitude);
+
+        OkHttpClient okHttpClient = new OkHttpClient();
+
+        Request req = new Request.Builder()
+                .url("http://api.openweathermap.org/data/2.5/weather?lat=51.03&lon=2.38&appid=d81df31027a2b337df5776d7cb7eb71e")
+                .build();
+
+        Response response = null;
+
+        okHttpClient.newCall(req).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                textLat.setText("erreur");
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                String result = response.body().string();
+
+                meteo.this.runOnUiThread(() -> textLat.setText(result));
+
+                final Gson gson = new Gson();
+                Rep rep = gson.fromJson(result, Rep.class);
+                Coordonnee coord = rep.getCoord();
+                coord.getLon();
+            }
+        });
     }
 }
